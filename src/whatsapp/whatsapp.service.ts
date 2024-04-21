ca57b39fd1f4e42ee9fef534f788d548dcbddab3
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2024-04-21 16:16:07 -0300

    feat: update puppeteer

--- src/whatsapp/whatsapp.service.ts
+++ src/whatsapp/whatsapp.service.ts
@@ -14,6 +14,10 @@ export class WhatsappService {
 
   constructor(private messageRepository: MessageRepository) {
     this.client = new Client({
+      puppeteer: {
+        headless: false,
+        executablePath: '/usr/bin/google-chrome',
+      },
       authStrategy: new LocalAuth(),
       webVersionCache: {
         type: 'remote',
